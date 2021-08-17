6cba26fde854a13d168d74607791aeea33af2e61
Biscuit <smonsalvel@eafit.edu.co>
2021-08-17 02:41:44 -0500

    Prueba

--- proyecto/codigo/csv/main.py
+++ proyecto/codigo/csv/main.py
@@ -1,14 +1,28 @@
+import csv
 import os
 
-def load(dir, img):
-    reader = open(dir + img, 'r')
-    list1 = []
-    for i in reader.readlines():
-        list1.append(i.split(","))
-    return list1
+dir1 = "C:/Users/USUARIO/Desktop/ST0245-001/proyecto/codigo/csv/sano_csv/"
+dir2 = "C:/Users/USUARIO/Desktop/ST0245-001/proyecto/codigo/csv/enfermo_csv/"
+list1 = os.listdir(dir1)
+list2 = os.listdir(dir2)
 
-directory = "C:/Users/USUARIO/Desktop/ST0245-001/proyecto/codigo/csv/sano_csv"
-img_list = os.listdir(directory)
-for i in img_list:
-    imglst = load(directory, i)
+def load(dlist, directory):
+    csv_list = []
+    for i in dlist:
+        with open(directory + i, 'r') as csv_file:
+            csv_list.append(csv.reader(csv_file))
+    return csv_list
+
+healthy = load(list1, dir1)
+sick = (list2, dir2)
+
+for i in healthy:
+    print(i)
+
+
+    
+
+
+
+
  
